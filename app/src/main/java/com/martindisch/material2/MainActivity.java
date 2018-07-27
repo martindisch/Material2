93686663a8c281ac2eb3e3ab3289591ac759c794
Martin Disch <martindisch@gmail.com>
2018-07-27 16:43:14 +0200

    Use proper way to show menu and drawer icons
    Because a real overflow menu would show a disgusting drop shadow over
    the navigation bar, we can only show a single icon and have to manually
    present a bottom sheet with the options.
    As for the hamburger icon, it only has a ripple effect (is clickable)
    when there is an OnClickListener set.

--- app/src/main/java/com/martindisch/material2/MainActivity.java
+++ app/src/main/java/com/martindisch/material2/MainActivity.java
@@ -1,14 +1,28 @@
 package com.martindisch.material2;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomAppBar mBottomAppBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomAppBar = findViewById(R.id.bar);
+        mBottomAppBar.inflateMenu(R.menu.bottom_bar_menu);
+        mBottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: show navigation
+            }
+        });
     }
 }
