98cbc00bdfd95f4b3f7163610bb1c87c70142a71
Martin Disch <martindisch@gmail.com>
2018-07-29 13:16:47 +0200

    Remove MainViewModel for simplicity

--- app/src/main/java/com/martindisch/material2/MainActivity.java
+++ app/src/main/java/com/martindisch/material2/MainActivity.java
@@ -6,9 +6,6 @@ import android.view.View;
 import com.google.android.material.bottomappbar.BottomAppBar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,25 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
-            // Acquire ViewModel
-            MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-            // Register Observer to replace Fragment when necessary
-            viewModel.getCurrentFragment().observe(this, new Observer<Integer>() {
-                @Override
-                public void onChanged(Integer fragment_id) {
-                    Fragment fragment = null;
-                    switch (fragment_id) {
-                        case MainViewModel.FRAGMENT_USER:
-                            fragment = UserFragment.newInstance();
-                            break;
-                        case MainViewModel.FRAGMENT_LIST:
-                            // TODO
-                            fragment = null;
-                            break;
-                    }
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
-                }
-            });
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, UserFragment.newInstance()).commit();
         }
 
         BottomAppBar bottomAppBar = findViewById(R.id.bar);
