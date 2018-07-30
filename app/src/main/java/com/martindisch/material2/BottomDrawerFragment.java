1860ffa7c41b88ac9fdc4abe31e672e5b8aaab78
Martin Disch <martindisch@gmail.com>
2018-07-30 10:27:56 +0200

    Make bottom sheet expand fully even in landscape

--- app/src/main/java/com/martindisch/material2/BottomDrawerFragment.java
+++ app/src/main/java/com/martindisch/material2/BottomDrawerFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.navigation.NavigationView;
 
@@ -21,6 +22,13 @@ public class BottomDrawerFragment extends BottomSheetDialogFragment {
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_bottom_drawer, container, false);
         navigationView = view.findViewById(R.id.bottom_drawer_navigation);
+
+        // Hack to get the sheet to fully expand in landscape (peek height is only about one list item by default)
+        getDialog().setOnShowListener(dialog ->
+        {
+            View bottomSheetInternal = getDialog().findViewById(R.id.design_bottom_sheet);
+            BottomSheetBehavior.from(bottomSheetInternal).setState(BottomSheetBehavior.STATE_EXPANDED);
+        });
         return view;
     }
 
