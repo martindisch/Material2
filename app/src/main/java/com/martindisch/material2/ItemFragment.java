01ac8b58bd662da2f68163aa24752a153377e0b2
Martin Disch <martindisch@gmail.com>
2018-08-01 11:59:54 +0200

    Add basic swipe to delete

--- app/src/main/java/com/martindisch/material2/ItemFragment.java
+++ app/src/main/java/com/martindisch/material2/ItemFragment.java
@@ -6,15 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.LinkedList;
+
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ItemFragment extends Fragment implements ItemAdapter.ItemCallback {
 
     private ItemAdapter mAdapter;
+    ItemListViewModel mViewModel;
 
     public static ItemFragment newInstance() {
         return new ItemFragment();
@@ -25,9 +29,32 @@ public class ItemFragment extends Fragment implements ItemAdapter.ItemCallback {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_item, container, false);
-        // Set adapter
+
+        // Prepare RecyclerView
         RecyclerView recyclerView = view.findViewById(R.id.rvItems);
+        // Add separator
         recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+        // Add swipe to delete
+        ItemTouchHelper touchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.START | ItemTouchHelper.END) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                // Get list of items
+                LinkedList<Item> items = (LinkedList<Item>) mViewModel.getItems().getValue();
+                // Clone it so we don't insert into the existing instance (DiffUtil wouldn't find difference)
+                LinkedList<Item> newItems = (LinkedList<Item>) items.clone();
+                // Remove item
+                newItems.remove(position);
+                mViewModel.getItems().setValue(newItems);
+            }
+        });
+        touchHelper.attachToRecyclerView(recyclerView);
+        // Set adapter
         mAdapter = new ItemAdapter(this);
         recyclerView.setAdapter(mAdapter);
         return view;
@@ -36,8 +63,8 @@ public class ItemFragment extends Fragment implements ItemAdapter.ItemCallback {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ItemListViewModel viewModel = ViewModelProviders.of(getActivity()).get(ItemListViewModel.class);
-        subscribeUi(viewModel);
+        mViewModel = ViewModelProviders.of(getActivity()).get(ItemListViewModel.class);
+        subscribeUi(mViewModel);
     }
 
     private void subscribeUi(ItemListViewModel viewModel) {
